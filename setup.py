c3b294c98b8dbf461aeb6fa790a43bbaebcc3cb9
Ameya Daigavane <ameya.d.98@gmail.com>
2019-11-06 20:38:40 +0530

    Added setup.py for pip.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysclump",
+    version="0.0.1",
+    author="Ameya Daigavane",
+    author_email="ameya.d.98@gmail.com",
+    description="SClump implemented in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ameya98/PySClump",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
